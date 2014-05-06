680ee7c908a088b3941e4bab49e8968fcdbd6246
Ingvar Stepanyan <me@rreverser.com>
2014-05-06 13:58:58 +0300

    No need to use nodeunit here.

--- test.js
+++ test.js
@@ -1,17 +1,15 @@
+var assert = require("assert");
+
 require("./"); // inject stack-displayname
 
-exports.test = function (test) {
-	var f = function () {
-		throw new Error();
-	};
-
-	f.displayName = "super puper function";
-
-	try {
-		f();
-	} catch (e) {
-		test.ok(e.stack.indexOf(f.displayName) >= 0);
-	} finally {
-		test.done();
-	}
+var f = function () {
+	throw new Error();
 };
+
+f.displayName = "super puper function";
+
+assert.throws(f, function (e) {
+	return e.stack.indexOf(f.displayName) >= 0;
+});
+
+console.log("Test passed.");
